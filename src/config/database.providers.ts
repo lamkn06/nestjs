72570267f9467a88dde6f7cb63b738614ed35381
lam.khuong <lamkn06@gmail.com>
2022-07-21 15:34:14 +0700

    chore: implement DB connection

---
+++ src/config/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT) || 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: process.env.MODE === 'production',
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
